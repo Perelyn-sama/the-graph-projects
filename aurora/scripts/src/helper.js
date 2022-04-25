9dab3011641839419de64d28897d12b00972ed48
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-04-25 23:28:09 +0100

    still not working

--- aurora/scripts/src/helper.js
+++ aurora/scripts/src/helper.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const express = require("express");
 const Web3 = require("web3");
 const cors = require("cors");
@@ -6,33 +7,45 @@ const BRLMasterChef = require("../abis/BRLMasterChef.json");
 require("dotenv").config(); // install me oooooooo
 // Get rpc url for aurora o
 
+const getCurrentblock = async () => {
+  const now = new Date();
+  const uinx = Math.round(now / 1000.0);
+  const currentBlock = await axios
+    .get(
+      `https://api.aurorascan.dev/api?module=block&action=getblocknobytime&timestamp=${uinx}&closest=before&apikey=YourApiKeyToken`
+    )
+    .then((res) => res.data.result);
+  // console.log(currentBlock);
+};
+
 async function main() {
   const BRL_CHEF_ADDR = "0x35CC71888DBb9FfB777337324a4A60fdBAA19DDE";
-
   const network = process.env.AURORA_NETWORK;
   const web3 = new Web3(new Web3.providers.HttpProvider(network));
   const instance = new web3.eth.Contract(BRLMasterChef, BRL_CHEF_ADDR);
 
-  //   const currentBlock = await instance.methods.getBlockNumber();
+  const currentBlock = getCurrentblock();
 
-  //   const multiplier = await instance.getMultiplier(
-  //     currentBlock,
-  //     currentBlock + 1
-  //   );
+  const multiplier = instance.methods.getMultiplier(
+    currentBlock,
+    currentBlock + 1
+  );
 
-  //   const rewardsPerWeek =
-  //     (((await instance.BRLPerBlock()) / 1e18) * multiplier * 604800) / 1.1;
+  // const rewardsPerWeek =
+  //   (((await instance.methods.BRLPerBlock()) / 1e18) * multiplier * 604800) /
+  //   1.1;
 
-  //   return rewardsPerWeek;
-  //   return instance.methods.getBlockNumber();
-  //   return currentBlock;
-  //   console.log(instance.methods);
-  //   console.log(web3.eth.getBlockNumber().then((res) => res));
-  //   console.log(num);
-  const num = web3.eth.getBlockNumber();
-  return num;
+  // console.log(rewardsPerWeek);
+  return instance.methods.owner().call();
 }
 
-// console.log(main().then((res) => res));
+// console.log(main());
+const network = process.env.AURORA_NETWORK;
 
-console.log(main());
+const web3 = new Web3(new Web3.providers.HttpProvider(network));
+const MyContract = new web3.eth.Contract(
+  BRLMasterChef,
+  "0x35cc71888dbb9ffb777337324a4a60fdbaa19dde"
+);
+// console.log(MyContract.methods.getMultiplier(64271292, 64271293).call());
+console.log(MyContract.methods.getMultiplier(64271292, 64271293));
