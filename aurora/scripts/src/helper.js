10b2670358cf8a005a486ff7d26cdb2adfa39a87
Perelyn-sama <64838956+Perelyn-sama@users.noreply.github.com>
2022-04-26 16:24:11 +0100

    calls work now

--- aurora/scripts/src/helper.js
+++ aurora/scripts/src/helper.js
@@ -1,68 +1,33 @@
-const axios = require("axios");
-const express = require("express");
 const Web3 = require("web3");
-const cors = require("cors");
 const BRLMasterChef = require("../abis/BRLMasterChef.json");
+const BRLToken = require("../abis/BRLToken.json");
 const BigNumber = require("bignumber.js");
 
-require("dotenv").config(); // install me oooooooo
-// Get rpc url for aurora o
-
-const getCurrentblock = async () => {
-  const now = new Date();
-  const uinx = Math.round(now / 1000.0);
-  const currentBlock = await axios
-    .get(
-      `https://api.aurorascan.dev/api?module=block&action=getblocknobytime&timestamp=${uinx}&closest=before&apikey=YourApiKeyToken`
-    )
-    .then((res) => res.data.result);
-  // console.log(currentBlock);
-};
-
-async function getMultiplier() {
-  const network = process.env.AURORA_NETWORK;
-  const web3 = new Web3(new Web3.providers.HttpProvider(network));
-  const MyContract = new web3.eth.Contract(
-    BRLMasterChef,
-    "0x35cc71888dbb9ffb777337324a4a60fdbaa19dde"
-  );
-
-  let res = await MyContract.methods.getMultiplier(64271292, 64271293).call();
-  return res;
-}
-
-async function getBRLPerBlock() {
-  const network = process.env.AURORA_NETWORK;
-  const web3 = new Web3(new Web3.providers.HttpProvider(network));
-  const MyContract = new web3.eth.Contract(
-    BRLMasterChef,
-    "0x35cc71888dbb9ffb777337324a4a60fdbaa19dde"
-  );
-
-  let res = await MyContract.methods.BRLPerBlock().call();
-  return res;
-}
+require("dotenv").config();
 
 async function main() {
   const BRL_CHEF_ADDR = "0x35CC71888DBb9FfB777337324a4A60fdBAA19DDE";
   const network = process.env.AURORA_NETWORK;
+
   const web3 = new Web3(new Web3.providers.HttpProvider(network));
-  const instance = new web3.eth.Contract(BRLMasterChef, BRL_CHEF_ADDR);
+  const BRL_CHEF = new web3.eth.Contract(BRLMasterChef, BRL_CHEF_ADDR);
 
-  const currentBlock = await getCurrentblock();
+  const rewardTokenAddress = await BRL_CHEF.methods.BRL().call();
+  const BRL_TKN = new web3.eth.Contract(BRLToken, rewardTokenAddress);
+  const BRL_DECIMALS = await BRL_TKN.methods.decimals().call();
 
-  const multiplier = await getMultiplier();
+  const currentBlock = await web3.eth.getBlockNumber();
 
-  const BRLPerBlock = await getBRLPerBlock();
+  const multiplier = await BRL_CHEF.methods
+    .getMultiplier(currentBlock, currentBlock + 1)
+    .call();
 
-  // return BRLPerBlock;
+  const BRLPerBlock = await BRL_CHEF.methods.BRLPerBlock().call();
 
   const rewardsPerWeek = ((BRLPerBlock / 1e18) * multiplier * 604800) / 1.1;
+  const other = ((BRLPerBlock / 10 ** BRL_DECIMALS) * 604800) / 3;
 
-  // console.log(BRLPerBlock);
-  // console.log(1e18);
-
+  console.log(other);
   return rewardsPerWeek;
 }
 main().then((res) => console.log(res));
-// getBRLPerBlock().then((res) => console.log(res));
